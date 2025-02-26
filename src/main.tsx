7ce9191be0d32a2303ed898d1bf549c212f02386
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-02-25 17:53:13 -0800

    added css fix for map markers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
 
-createRoot(document.getElementById('root')!).render(
+// Required to make maplibre-gl markers work
+import "maplibre-gl/dist/maplibre-gl.css"
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
