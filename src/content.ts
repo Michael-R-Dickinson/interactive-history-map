532355db0faf016b7ad6a443ecda454386133dc3
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-03-15 20:56:54 -0700

    finalized wagadou entry

--- src/content.ts
+++ src/content.ts
@@ -5,24 +5,24 @@ const wagadou: LocationData = {
   location: "Western Africa",
   name: "Wagadou",
   path: "wagadou",
-  dateRange: "300-1200 AD (placeholder)",
+  dateRange: "200 to 1200 CE",
   overview:
     "The Kingdom of Wagadou was a powerful cosmopolitan center during the 11th century. Known for its extensive gold reserves, it conducted frequent trade with North Africa, exchanging goods, ideas and culture.",
   descriptionParagraphs: [
-    "The Kingdom of Wagadou was a thriving economic and cultural center in the 11th century, recognized across the Islamic world for its vast gold reserves. Its location along major trade routes facilitated frequent exchanges with North Africa, importing goods such as salt and textiles in return for gold. Additionally, the kingdom's precise taxation system capitalized on of this network, allowing for prosperous trade while enriching the central government. This wealth brought the kingdom into contact with merchants and scholars from Islamic North Africa and beyond. ",
-    "The city stands out for its harmonious co-existence between people of many religions and beliefs. Practitioners of Islam and the region's traditional religion cooperated at all levels of government and commerce creating a unique cultural blend in the kingdom.",
+    "The Kingdom of Wagadou was a thriving economic and cultural center in the 11th century, recognized across the Islamic world for its vast gold reserves. Its location along major trade routes facilitated frequent exchanges with North Africa, importing goods such as salt and textiles in return for gold. Additionally, the kingdom's precise taxation system capitalized on this network, allowing for prosperous trade while enriching the central government. This wealth brought the kingdom into contact with merchants and scholars from Islamic North Africa and beyond. ",
+    "The city stands out for its harmonious co-existence between people of many religions and beliefs. Practitioners of Islam and the region's traditional religion cooperated at all levels of government and commerce, creating a unique cultural blend in the kingdom.",
   ],
   lifeAtLocationSection: {
-    caption: "Section Title Incoming",
+    caption: "Life in the Kingdom of Wagadou",
     paragraphs: [
-      "Wagadou's religious inclusivity is reflected in the city's layout and design. Instead of blending religious spaces with government or workplace buildings as was common in other societies, Wagadou created separation between these functions as to avoid favoring one religion over another. Religions could construct their own houses of worship and delegate these spaces for religious practice, maintaining a secular workplace or governmental environment. ",
+      "Wagadou's religious inclusivity is reflected in the city's layout and design. Instead of blending religious spaces with government or workplace buildings as was common in other societies, Wagadou created separation between these functions to avoid favoring one religion over another. Religions could construct their own houses of worship and delegate these spaces for religious practice, maintaining a secular workplace or governmental environment. ",
       "Still, the government showed support for traditional religions, sponsoring the construction of designated spaces for shamans of the region's traditional beliefs. This environment that both supported traditional religions and included those of other belief systems likely allowed for the prosperous coexistence of many religions in the area. ",
     ],
   },
   globalSignificanceSection: {
-    caption: "Section Title 2 Incoming",
+    caption: "Connections to Trans-Saharan Trade",
     paragraphs: [
-      "Wagadou was highly connected to a larger global network, not only through trade but also through cultural and religious exchange. Islam’s emergence in the area was likely the result of the their extensive trade network across Africa, where religious beliefs moved alongside goods. These trade networks are highly global as they facilitated the spread of both merchandise and culture from various cities across Africa. ",
+      "Wagadou was highly connected to a larger global network, not only through trade but also through cultural and religious exchange. Islam’s emergence in the area was likely the result of their extensive trade network across Africa, where religious beliefs moved alongside goods. These trade networks are highly global as they facilitated the spread of both merchandise and culture from various cities across Africa. ",
     ],
   },
   latitude: 14.45,
@@ -60,38 +60,40 @@ const catahoyuk: LocationData = {
   longitude: 32.8257,
 }
 
-const locationsInfo: LocationData[] = [
-  wagadou,
-  catahoyuk,
-  {
-    imageFileName: "fire_temple.jpg",
-    location: "Iran",
-    name: "Zoroastrian Fire Temples",
-    path: "fire_temples",
-    dateRange: "300-1200 AD (placeholder)",
-    overview:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-    descriptionParagraphs: [
+const fire_temples: LocationData = {
+  imageFileName: "fire_temple.jpg",
+  location: "Iran",
+  name: "Zoroastrian Fire Temples",
+  path: "fire_temples",
+  dateRange: "300-1200 AD (placeholder)",
+  overview:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+  descriptionParagraphs: [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  ],
+  lifeAtLocationSection: {
+    caption: "Section Title Incoming",
+    paragraphs: [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     ],
-    lifeAtLocationSection: {
-      caption: "Section Title Incoming",
-      paragraphs: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      ],
-    },
-    globalSignificanceSection: {
-      caption: "Section Title Incoming",
-      paragraphs: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      ],
-    },
-    latitude: 29.6121,
-    longitude: 52.5311,
   },
+  globalSignificanceSection: {
+    caption: "Section Title Incoming",
+    paragraphs: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    ],
+  },
+  latitude: 29.6121,
+  longitude: 52.5311,
+}
+
+const locationsInfo: LocationData[] = [
+  wagadou,
+  catahoyuk,
+  fire_temples,
   {
     imageFileName: "cordoba.jpg",
     location: "Cordoba",
