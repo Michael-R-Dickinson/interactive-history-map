1dab14ddc8b15af212a1faacbb0eef44d16ff2fa
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-03-18 01:22:52 -0700

    added zoroastrian fire temples content

--- src/content.ts
+++ src/content.ts
@@ -62,31 +62,29 @@ const catahoyuk: LocationData = {
 
 const fire_temples: LocationData = {
   imageFileName: "fire_temple.jpg",
-  location: "Iran",
+  location: "Achaemenid Empire",
   name: "Zoroastrian Fire Temples",
   path: "fire_temples",
-  dateRange: "300-1200 AD (placeholder)",
+  dateRange: "3rd to 7th century CE",
   overview:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+    "Zoroastrian Fire Temples were cultural and religious hubs in the Achaemenid empire. Built to promote the religion, these temples helped Zoroastrianism reach a wide audience and spread its practice across the empire.",
   descriptionParagraphs: [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "During the 5th century, Zoroastrianism was widespread across the Achaemenid empire with practitioners congregating in fire temples for worship and ceremonies. These temples served as the heart of Zoroastrian communities, where worshippers gathered to honor Ahura Mazda, the supreme deity of Zoroastrianism. Inside the temples priests maintained a sacred flame, stoking it regularly to ensure it stayed alight. The flame symbolized purity, helping to remind and encourage practitioners of the religion's values such as cleanliness and moral righteousness. ",
   ],
   lifeAtLocationSection: {
     caption: "Section Title Incoming",
     paragraphs: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "At the heart of Zoroastrian worship and teachings is the Yasna, one of the most important guiding texts of the religion. The Yasna is a piece of the larger set of Zoroastrian texts known as the Avesta. These began as a memorized oral tradition, but during the Achaemenid and Sasanian empires, they were collected into written records.",
+      'The text emphasizes a strict moral code to be followed in daily life, placing extraordinary importance on righteous behavior in both thoughts and actions. The Yasna explicitly praises "good thoughts, good words, and good deeds," establishing these as fundamental principles for followers to uphold in their everyday lives. These principles imply that followers should be honest and candid in their communications with others, as expressed by "good words"; they should avoid deceptive or negative thoughts, as conveyed by "good thoughts"; and they should contribute to the general welfare of the world by conducting themselves morally, as expressed by "good deeds."',
     ],
   },
   globalSignificanceSection: {
     caption: "Section Title Incoming",
     paragraphs: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Fire temples are global for the way they spread architectural styles across the Achaemenid empire. These temples used architecture to emphasize the religion's values, leading to a distinctive temple design. As Zoroastrianism spread, new fire temples were constructed by its followers, spreading the architectural style across the empire. The propagation of fire temple architecture represents a global development for its transfer of architectural ideas through the network of Zoroastrian followers. Inversely, each fire temple location served as a hub",
     ],
   },
-  latitude: 29.6121,
+  latitude: 33.6121,
   longitude: 52.5311,
 }
 
