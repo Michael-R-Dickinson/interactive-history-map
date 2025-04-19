0a07eba5cab9a7b2f42f052ec5ebfdbe56fbf0db
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2025-04-18 17:52:29 -0700

    added kingdom of mali content

--- src/content/taghaza.ts
+++ src/content/taghaza.ts
@@ -5,25 +5,23 @@ export const taghaza: LocationData = {
   location: "Western Sahara",
   name: "Kingdom of Mali",
   path: "taghaza",
-  dateRange: "300-1200 AD (placeholder)",
+  dateRange: "1226 - 1610 CE",
   overview:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+    "The Kingdom of Mali was a powerful economic hub around the 14th century, facilitating massive trade in the Western Sahara region. Propelled by leaders like Mansa Musa, the kingdom's wealth and status grew and became renowned across the Islamic world. ",
   descriptionParagraphs: [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "During the 14th century, the Kingdom of Mali was at its peak. Fueled by extreme wealth in gold, its reach expanded to as far as the Atlantic for around twelve hundred miles. It was responsible for the creation of numerous unique pieces of monumental architecture such as the Timbuktu mosques. ",
+    "Additionally, the empire established a powerful trade network for creating and exporting goods and raw materials from as far as the Atlantic. This network relied on slave labour for the mass production of certain materials such as salt and gold. Slave settlements were created for this purpose, establishing mines such as the ones at Taghaza, where large quantities of salt were extracted and exported to the kingdom. ",
   ],
   lifeAtLocationSection: {
-    caption: "Section Title Incoming",
+    caption: "Pragmatic Architecture in the Saharan Climate",
     paragraphs: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "The settlement at Taghaza was comprised primarily of slaves, with little access to expensive resources or building materials. Instead, they maximized the resources available locally to construct buildings for housing and community. For example, a Moroccan traveler in the area, Ibn Battuta, noted in his writings that houses and mosques in Taghaza were constructed with blocks of salt and covered with camel skins. Because of the extremely arid climate in the western Sahara, these salt blocks were surprisingly durable building materials, withstanding the rare rain with their waterproof camel skin coverings. Although these salt blocks would have been valuable when sold across the empire, in Taghaza they were commonplace and one of the few possible building materials in the area. The inhabitants' choice to build with these blocks represents a unique adaptation by the locals to their environment, allowing them to maximize the available resources to survive in the Sahara’s harsh climate.",
     ],
   },
   globalSignificanceSection: {
-    caption: "Section Title Incoming",
+    caption: "Integration with Malian Trade Networks",
     paragraphs: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "While traveling through the area, Ibn Battuta remarked on the export of these salt blocks from mining towns like Taghaza to the rest of the Kingdom of Mali. He observes that these blocks would be taken across the empire and sold at various cities for high prices. From this precise information about the salt prices in various regions, we can infer that Taghaza established trade deals for consistent salt prices at these locations. This indicates that the stream of salt from Taghaza through this network was frequent enough to warrant ongoing trade relationships. These relationships and the flow of goods from production centers like Taghaza to the major cities in the Kingdom of Mali constitute a global trade network for their connectivity across a wide area in the Sahara.",
     ],
   },
   latitude: 20.7856,
